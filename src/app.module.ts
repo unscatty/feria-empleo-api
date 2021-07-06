e4064cf6abf44af05789b0a82a3b9a27e1109e25
David <david@raccoondev.studio>
2021-07-05 21:25:21 -0500

    starting basic configuration and get connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { EnvConfig } from './config/config.keys';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+
+  constructor(private readonly _configService: ConfigService) {
+    AppModule.port = this._configService.get(EnvConfig.PORT);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { CustomLogger } from './library/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new CustomLogger('Main'); // Main here is just the context
+
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
+
+  app.enableCors();
+
+  app.use(morgan('tiny'));
+  const port = AppModule.port || 3000;
+  await app.listen(port, () => {
+    logger.log('Listening at http://localhost:' + port);
+  });
 }
 bootstrap();
