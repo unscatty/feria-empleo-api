0d60c181de9d83625de2bba581484714c5fb5b6d
Carlos Aguilera <ckarlos_95@hotmail.es>
2021-08-14 17:30:31 -0500

    Add file upload to company invitation

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -1,6 +1,10 @@
+import { AzureStorageModule, AzureStorageService } from '@nestjs/azure-storage';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvConfig } from 'src/config/config.keys';
 import { EmailService } from 'src/core/providers/mail/email.service';
+import { UploadedImage } from 'src/shared/entitities/uploaded-image.entity';
 import { Role } from '../user/entities/role.entity';
 import { User } from '../user/entities/user.entity';
 import { UserModule } from '../user/user.module';
@@ -10,8 +14,24 @@ import { CompanyService } from './company.service';
 import { Company } from './entities/company.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Company, Role]), UserModule],
+  imports: [
+    TypeOrmModule.forFeature([User, Company, Role, UploadedImage]),
+    UserModule,
+    AzureStorageModule.withConfigAsync({
+      useFactory: (config: ConfigService) => ({
+        sasKey: config.get(EnvConfig.AZURE_STORAGE_SAS_KEY),
+        accountName: config.get(EnvConfig.AZURE_STORAGE_ACCOUNT),
+        containerName: config.get(EnvConfig.AZURE_STORAGE_CONTAINER_NAME),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [CompanyController],
-  providers: [CompanyService, EmailService, CompanyEmailService],
+  providers: [
+    CompanyService,
+    EmailService,
+    CompanyEmailService,
+    AzureStorageService,
+  ],
 })
 export class CompanyModule {}
--- src/modules/company/dto/create-company.dto.ts
+++ src/modules/company/dto/create-company.dto.ts
@@ -7,4 +7,7 @@ export class CreateCompanyDto {
 
   @IsNotEmpty()
   name: string;
+
+  @IsOptional()
+  imageURL?: string;
 }
