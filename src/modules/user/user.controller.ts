613fc56c7db74b6ce193b52d11bec9e71b5a9c96
Carlos Aguilera <ckarlos_95@hotmail.es>
2021-10-17 22:21:06 -0500

    Fix admin serialization when getting current user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  SerializeOptions,
+} from '@nestjs/common';
+import { classToPlain } from 'class-transformer';
 import { Allow } from '../auth/decorators/role.decorator';
 import { GetUser } from '../auth/decorators/user.decorator';
 import { Candidate } from '../candidate/models/candidate.entity';
 import { Company } from '../company/entities/company.entity';
 import { CreateUserDto, FilterUsersDto } from './dto';
+import { RoleGroup } from './entities/role.entity';
 import { RoleType, User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -31,10 +42,12 @@ export class UserController {
 
   // Serialize admin for an homogeneous response
   @Get('current-user')
-  currentUser(@GetUser() user: User): Promise<Company | Candidate> | { user: User } {
+  // WARN: This should be the only place where CURRENT_USER group must be used
+  @SerializeOptions({ groups: [RoleGroup.CURRENT_USER] })
+  async currentUser(@GetUser() user: User): Promise<Candidate | Company | { user: User }> {
     switch (user.role.name) {
       case RoleType.ADMIN:
-        return { user };
+        return { user: classToPlain(user) as User };
       case RoleType.CANDIDATE:
         return this.userService.getCandidate(user);
       case RoleType.COMPANY:
